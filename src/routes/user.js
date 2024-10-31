ad6bf5e2b4f011a370c6f1fdc1a5d3b4406e8355
Ayush <ayushagarwal.dev@gmail.com>
2024-10-31 15:05:20 +0530

    add fedd endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const router = express.Router();
+
+const USER_DATA_TO_SEND = [
+  "firstName",
+  "lastName",
+  "avatar_url",
+  "skills",
+  "gender",
+  "about",
+  "age",
+];
 
 // Get all the pending connection request for loggedIn user
 router.get("/requests/received", userAuth, async (req, res) => {
@@ -12,19 +23,11 @@ router.get("/requests/received", userAuth, async (req, res) => {
     const connectionRequest = await ConnectionRequest.find({
       toUserId: loggedInUser.userId,
       status: "interested",
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "avatar_url",
-      "skills",
-      "gender",
-      "about",
-      "age",
-    ]);
+    }).populate("fromUserId", USER_DATA_TO_SEND);
 
     res.status(200).json({ data: connectionRequest });
   } catch (err) {
-    res.status(400).json({ message: "Something went wrong" });
+    res.status(500).json({ message: "Something went wrong" });
   }
 });
 
@@ -48,7 +51,36 @@ router.get("/connections", userAuth, async (req, res) => {
     });
     res.status(200).json({ data });
   } catch (err) {
-    res.status(400).json({ message: "Something went wrong" });
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+router.get("/feed", userAuth, async (req, res) => {
+  const loggedInUser = req.user;
+
+  try {
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser.userId },
+        { toUserId: loggedInUser.userId },
+      ],
+    }).select(["fromUserId", "toUserId"]);
+
+    const usersWithRequestConnectionCreated = new Set();
+    connectionRequests.forEach((row) => {
+      usersWithRequestConnectionCreated.add(row.fromUserId.toString());
+      usersWithRequestConnectionCreated.add(row.toUserId.toString());
+    });
+
+    usersWithRequestConnectionCreated.add(loggedInUser.userId.toString());
+
+    const users = await User.find({
+      _id: { $nin: Array.from(usersWithRequestConnectionCreated) },
+    }).select(USER_DATA_TO_SEND);
+
+    res.status(200).json({ data: users });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
   }
 });
 
