1057274342f0a700c8cb8a9e15f0e34d529f577d
Ayush <ayushagarwal.dev@gmail.com>
2024-11-02 15:56:15 +0530

    add cors middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const connectDB = require("./config/database");
@@ -11,6 +12,7 @@ const userRouter = require("./routes/user");
 
 const PORT = 3000;
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
